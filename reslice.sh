6904dc49cf04c251533cfa5e6f7f606f70a022d9
Brad Caron <bacaron@utexas.edu>
2022-10-27 16:16:27 -0500

    reason: initial commit

---
+++ reslice.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+input=`jq -r '.input' config.json`
+voxel_dim=`jq -r '.voxel_dim' config.json`
+TYPE=`jq -r '.type' config.json`
+
+[ ! -d output ] && mkdir -p output && outdir="./output"
+
+[ ! -f ${outdir}/${type}.nii.gz ] && flirt -in ${input} -ref ${input} -out ${outdir}/${type}.nii.gz -applyisoxfm ${voxel_dim}
+
+[ -f ${outdir}/${type}.nii.gz ] && echo "complete" && exit 0 || echo "something went wrong" && exit 1
